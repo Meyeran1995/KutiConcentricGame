1096781c5308dbce916a5db29f8f2d7fd5e7a537
Andre.Meyer <Andre.Meyer@haw-hamburg.de>
2022-11-09 15:20:05 +0100

    feat: Playermodels move inward on order change

--- Assets/Code/Scripts/Player/PlayerOrder.cs
+++ Assets/Code/Scripts/Player/PlayerOrder.cs
@@ -10,11 +10,12 @@ namespace Meyham.Player
         [SerializeField] private SpriteRenderer spriteRenderer;
         [SerializeField] private PlayerCollisionHelper collisionHelper;
 
-
+        private const float OrderDisplacementAmount = 32f / 100f;
+        
         public void OrderPlayer(int order)
         {
             spriteRenderer.sprite = modelProvider.GetModel(order);
-            
+            transform.localPosition = new Vector3(-order * OrderDisplacementAmount, 0f, 0f);
             collisionHelper.ModifyCollisionSize(sizeFactor.RuntimeValue, order);            
         }
     }
