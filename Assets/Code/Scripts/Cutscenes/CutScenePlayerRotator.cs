9652fac2cc7ecfff7cd2c89394da1fd610fda050
Andre Meyer <Andre.Meyer@haw-hamburg.de>
2023-03-01 15:50:12 +0100

    feat: Add objects for player sign in animation

---
+++ Assets/Code/Scripts/Cutscenes/CutScenePlayerRotator.cs
@@ -0,0 +1,98 @@
+using Meyham.DataObjects;
+using Meyham.EditorHelpers;
+using UnityEngine;
+
+namespace Meyham.Cutscenes
+{
+    public class CutScenePlayerRotator : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private Transform outerPivot;
+        
+        [Header("Values")]
+        [SerializeField] private FloatValue rotationSpeed;
+        [SerializeField] private FloatValue outerStartingRotation;
+        
+        [Header("Debug")]
+        [SerializeField, ReadOnly] private float lerpOuter;
+        [SerializeField, ReadOnly] private bool shouldRotateOuter;
+        [Space]
+        [SerializeField, ReadOnly] private float lerpInner;
+        [SerializeField, ReadOnly] private bool shouldRotateInner;
+        
+        private Quaternion desiredRotationOuter, desiredRotationInner;
+        
+        private Quaternion currentRotationOuter, currentRotationInner;
+
+        public void RotateIntoCircle()
+        {
+            desiredRotationOuter = Quaternion.identity;
+
+            shouldRotateOuter = true;
+            lerpOuter = 0f;
+            enabled = true;
+        }
+
+        public void RotateOutOfCircle()
+        {
+            desiredRotationOuter = Quaternion.AngleAxis(outerStartingRotation, Vector3.forward);
+
+            shouldRotateOuter = true;
+            lerpOuter = 0f;
+            enabled = true;
+        }
+
+        public void RotateTowardsCircleAngle(float angle)
+        {
+            desiredRotationInner = Quaternion.AngleAxis(angle, Vector3.forward);
+            shouldRotateInner = true;
+            lerpInner = 0f;
+            enabled = true;
+        }
+        
+        private void Awake()
+        {
+            currentRotationInner = Quaternion.identity;
+            currentRotationOuter = Quaternion.AngleAxis(outerStartingRotation, Vector3.forward);
+            outerPivot.localRotation = currentRotationOuter;
+        }
+
+        private void Update()
+        {
+            if (!shouldRotateOuter && !shouldRotateInner)
+            {
+                enabled = false;
+                return;
+            }
+            
+            if (shouldRotateInner)
+            {
+                lerpInner += Time.deltaTime * rotationSpeed.RuntimeValue;
+
+                if (lerpInner > 1f)
+                {
+                    lerpInner = 1f;
+                    shouldRotateInner = false;
+                }
+                
+                currentRotationInner = Quaternion.Lerp(currentRotationInner, desiredRotationInner, lerpInner);
+
+                transform.localRotation = currentRotationInner;
+            }
+            
+            if(!shouldRotateOuter) return;
+            
+            lerpOuter += Time.deltaTime * rotationSpeed.RuntimeValue;
+
+            if (lerpOuter > 1f)
+            {
+                lerpOuter = 1f;
+                shouldRotateOuter = false;
+            }
+                
+            currentRotationOuter = Quaternion.Lerp(currentRotationOuter, desiredRotationOuter, lerpOuter);
+
+            outerPivot.localRotation = currentRotationOuter;
+        }
+    }
+}
