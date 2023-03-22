af3b9b91d3ae09d9f7378227b5ffadda0760831a
André Meyer <andremeyer@1995@web.de>
2023-03-22 16:44:19 +0100

    refactor: Improve input code

---
+++ Assets/Code/Scripts/Input/InputConstants.cs
@@ -0,0 +1,14 @@
+﻿using Meyham.Player;
+
+namespace Meyham.Input
+{
+    public abstract class InputConstants
+    {
+        public const int P1LeftButton = (int)PlayerDesignation.Orange;
+        public const int P1MiddleButton = (int)PlayerDesignation.Green;
+        public const int P1RightButton = (int)PlayerDesignation.Purple;
+        public const int P2LeftButton = (int)PlayerDesignation.Yellow;
+        public const int P2MiddleButton = (int)PlayerDesignation.Red;
+        public const int P2RightButton = (int)PlayerDesignation.Cyan;
+    }
+}
