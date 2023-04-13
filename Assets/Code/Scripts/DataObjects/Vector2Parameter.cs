c1ca7a84e2d8d00610e5988c8871725c93badbc5
Andre Meyer <andremeyer1995@web.de>
2023-04-13 18:26:59 +0200

    chore: Missing data from Vector2Parameter commit

--- Assets/Code/Scripts/DataObjects/Vector2Parameter.cs
+++ Assets/Code/Scripts/DataObjects/Vector2Parameter.cs
@@ -5,8 +5,6 @@ namespace Meyham.DataObjects
     [CreateAssetMenu(menuName = "ScriptableObjects/DataObjects/Vector2Parameter")]
     public class Vector2Parameter : ParameterSO<Vector2>
     {
-        public float Magnitude => RuntimeValue.magnitude;
-        
         public static Vector2 operator -(Vector2Parameter a) => -a.RuntimeValue;
 
         public static Vector2 operator +(Vector2Parameter a, Vector2Parameter b) => a.RuntimeValue + b.RuntimeValue;
