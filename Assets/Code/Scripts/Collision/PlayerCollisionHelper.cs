aa3f72ce73e5a7e172cd968a3cbf28b24c19d434
Andre Meyer <andremeyer1995@web.de>
2023-06-26 18:11:22 +0200

    refactor: Made player collision resolver into static class

--- Assets/Code/Scripts/Collision/PlayerCollisionHelper.cs
+++ Assets/Code/Scripts/Collision/PlayerCollisionHelper.cs
@@ -3,37 +3,39 @@ using UnityEngine;
 
 namespace Meyham.Collision
 {
-    public class PlayerCollisionHelper : MonoBehaviour
+    public static class PlayerCollisionHelper
     {
         private static Transform origin;
         
-        private static LayerMaskProvider maskProvider;
+        private static readonly LayerMaskProvider MaskProvider = new ();
         
         private static readonly Dictionary<Collider, PlayerCollision> ColliderToPlayer = new(6);
 
         public static Vector3 GetOriginPos()
         {
+            origin ??= GameObject.FindGameObjectWithTag("Origin").transform;
+            
             return origin.position;
         }
 
         public static int GetMaskForLowerOrders(int playerOrder)
         {
-            return maskProvider.GetMaskForLowerOrders(playerOrder);
+            return MaskProvider.GetMaskForLowerOrders(playerOrder);
         }
         
         public static int GetMaskForHigherOrders(int playerOrder)
         {
-            return maskProvider.GetMaskForHigherOrders(playerOrder);
+            return MaskProvider.GetMaskForHigherOrders(playerOrder);
         }
         
         public static int GetMaskForSameOrder(int playerOrder)
         {
-            return maskProvider.GetMaskForSameOrder(playerOrder);
+            return MaskProvider.GetMaskForSameOrder(playerOrder);
         }
         
         public static int GetLayer(int playerOrder)
         {
-            return maskProvider.GetLayer(playerOrder);
+            return MaskProvider.GetLayer(playerOrder);
         }
 
         public static void Register(Collider playerCollider, PlayerCollision playerCollision)
@@ -44,12 +46,6 @@ namespace Meyham.Collision
         public static PlayerCollision GetPlayerByCollider(Collider playerCollider)
         {
             return ColliderToPlayer[playerCollider];
-        }
-        
-        private void Awake()
-        {
-            maskProvider = new LayerMaskProvider();
-            origin = GameObject.FindGameObjectWithTag("Origin").transform;
         }
     }
 }
