01981c3b52afaad24d6269123ac3ce98d8118e8b
Andre Meyer <andremeyer1995@web.de>
2024-01-19 19:32:53 +0100

    chore: Missing scripts from collectible rework

---
+++ Assets/Code/Scripts/Items/ItemCollectibleCarrier.cs
@@ -0,0 +1,22 @@
+﻿using Meyham.DataObjects;
+using Unity.Collections;
+using UnityEngine;
+
+namespace Meyham.Items
+{
+    public class ItemCollectibleCarrier : MonoBehaviour
+    {
+        [field: ReadOnly, SerializeField] 
+        public ACollectibleData Collectible { get; private set; }
+
+        public void SetCollectible(ACollectibleData collectibleData)
+        {
+            Collectible = collectibleData;
+        }
+
+        public void OnCollected(GameObject playerCollision)
+        {
+            Collectible.Collect(playerCollision);
+        }
+    }
+}
