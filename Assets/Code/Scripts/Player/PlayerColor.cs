7d9f2de8ced9f6f0848c0fd1a2137df77c7a2adb
Andre Meyer <andremeyer1995@web.de>
2024-02-01 18:31:12 +0100

    feat: Add player color module

---
+++ Assets/Code/Scripts/Player/PlayerColor.cs
@@ -0,0 +1,30 @@
+﻿using Meyham.Player.Bodies;
+using Meyham.Set_Up;
+using UnityEngine;
+
+namespace Meyham.Player
+{
+    public class PlayerColor : MonoBehaviour, IPlayerColorReceiver
+    {
+        private Color activeColor;
+
+        public void SetColor(int playerId, Color color)
+        {
+            activeColor = color;
+            
+            var playerBody = GetComponent<PlayerBody>();
+            playerBody.BodyPartAcquired += OnBodyPartAcquired;
+
+            foreach (var bodyPart in playerBody.GetBodyParts())
+            {
+                bodyPart.SetColor(activeColor);
+            }
+        }
+        
+
+        private void OnBodyPartAcquired(BodyPart bodyPart)
+        {
+            bodyPart.SetColor(activeColor);
+        }
+    }
+}
