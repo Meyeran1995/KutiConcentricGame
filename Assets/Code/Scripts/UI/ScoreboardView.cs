800ce75de8f000e6a8cef270c6f644593c6f04aa
Andre Meyer <andremeyer1995@web.de>
2024-02-18 19:00:57 +0100

    feat: Improved scoreboard animation

--- Assets/Code/Scripts/UI/ScoreboardView.cs
+++ Assets/Code/Scripts/UI/ScoreboardView.cs
@@ -1,122 +1,68 @@
-﻿using System.Collections;
-using DG.Tweening;
-using Meyham.Player;
+﻿using System.Collections.Generic;
+using Meyham.Events;
 using Meyham.Set_Up;
 using UnityEngine;
 
 namespace Meyham.UI
 {
-    public class ScoreboardView : AGameView, IPlayerColorReceiver, IPlayerNumberDependable
+    public class ScoreboardView : AGameView, IPlayerNumberDependable
     {
         [Header("References")]
+        [SerializeField] private GenericEventChannelSO<int> playerDestroyed;
+        [SerializeField] private PlayerColors playerColors;
+        [Space]
         [SerializeField] private ScoreBoardEntry[] scoreBoardEntries;
 
-        [Header("Parameters")] 
-        [SerializeField] private float maxCountUpTime;
-        [SerializeField] private float minCountUpTime;
+        private readonly List<int> placements = new();
 
-        private ScoreTweenWrapper[] activeTweens;
-
-        public void SetColor(int playerId, Color color)
-        {
-            scoreBoardEntries[playerId].SetEntryColor(color);
-        }
+        private int playerCount;
 
         public void OnPlayerJoined(int playerNumber)
         {
-            scoreBoardEntries[playerNumber].gameObject.SetActive(true);
+            scoreBoardEntries[playerCount].gameObject.SetActive(true);
+            playerCount++;
         }
 
         public void OnPlayerLeft(int playerNumber)
         {
-            scoreBoardEntries[playerNumber].gameObject.SetActive(false);
+            playerCount--;
+            scoreBoardEntries[playerCount].gameObject.SetActive(false);
         }
 
-        // public WaitForSeconds CountUpScores(PlayerScore[] playerScores)
-        // {
-        //     var targetScores = new int[playerScores.Length];
-        //     var winnerScore = playerScores[0].GetScore();
-        //     var winnerIndex = 0;
-        //     var loserScore = playerScores[0].GetScore();
-        //     var loserIndex = 0;
-        //
-        //     for (int i = 1; i < playerScores.Length; i++)
-        //     {
-        //         var score = playerScores[i].GetScore();
-        //         targetScores[i] = score;
-        //         
-        //         if (score > winnerScore)
-        //         {
-        //             winnerScore = score;
-        //             winnerIndex = i;
-        //             continue;
-        //         }
-        //
-        //         if (score >= loserScore) continue;
-        //         
-        //         loserScore = score;
-        //         loserIndex = i;
-        //     }
-        //
-        //     StartCoroutine(CountUpAnimation(targetScores, winnerIndex, loserIndex, playerScores));
-        //     return new WaitForSeconds(maxCountUpTime);
-        // }
+        public void SetUpPlacements()
+        {
+            for (var i = 0; i < placements.Count; i++)
+            {
+                scoreBoardEntries[i].SetEntryColor(playerColors[placements[i]]);
+            }
+        }
 
         public override void Clean()
         {
-            activeTweens = null;
-
-            foreach (var entry in scoreBoardEntries)
-            {
-                entry.SetScore("0");
-            }
+            placements.Clear();
+            //TODO: deactivate particles
         }
         
         protected override void Awake()
         {
-            foreach (var entry in scoreBoardEntries)
+            for (var i = 0; i < scoreBoardEntries.Length; i++)
             {
+                var entry = scoreBoardEntries[i];
                 entry.gameObject.SetActive(false);
+                entry.SetPlacement(i + 1);
             }
+
             base.Awake();
         }
 
-        // private IEnumerator CountUpAnimation(int[] targetScores, int winnerIndex, int loserIndex, PlayerScore[] playerScores)
-        // {
-        //     activeTweens = new ScoreTweenWrapper[targetScores.Length];
-        //
-        //     for (int i = 0; i < targetScores.Length; i++)
-        //     {
-        //         var score = targetScores[i];
-        //         var countUpInterpolationValue = Mathf.InverseLerp(targetScores[loserIndex], targetScores[winnerIndex], score);
-        //         activeTweens[i] = new ScoreTweenWrapper(score, Mathf.Lerp(minCountUpTime, maxCountUpTime, countUpInterpolationValue));
-        //     }
-        //
-        //     while (activeTweens[winnerIndex].IsActive)
-        //     {
-        //         for (var i = 0; i < playerScores.Length; i++)
-        //         {
-        //             var playerScore = playerScores[i];
-        //             scoreBoardEntries[(int)playerScore.Designation].SetScore(activeTweens[i].CurrentScore.ToString());
-        //         }
-        //
-        //         yield return null;
-        //     }
-        // }
-        
-        private class ScoreTweenWrapper
+        private void Start()
         {
-            private readonly Tweener tween;
+            playerDestroyed += OnPlayerDestroyed;
+        }
 
-            public int CurrentScore { get; private set; }
-
-            public bool IsActive => tween.active;
-            
-            public ScoreTweenWrapper(int desiredScore, float desiredDuration)
-            {
-                CurrentScore = 0;
-                tween = DOTween.To(() => CurrentScore, score => CurrentScore = score, desiredScore, desiredDuration);
-            }
+        private void OnPlayerDestroyed(int designation)
+        {
+            placements.Add(designation);
         }
     }
 }
